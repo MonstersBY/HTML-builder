ace0734f4adfd0f6ed9b144cd5a3539a6e48caf7
Alex Romanchenko <monsters.alex0@gmail.com>
2022-11-05 14:06:06 +0300

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+const fsPromises = require('fs').promises;
 
+var filePath = path.join(__dirname, 'files')
+var copyFilePath = path.join(__dirname, 'files-copy')
+
+if (!fs.existsSync(copyFilePath)){
+    fs.mkdirSync(copyFilePath)
+}
+
+copyDir()
+function copyDir() {
+    fs.readdir(copyFilePath, (err, files)=>{
+        files.forEach(file => {
+            fs.unlinkSync(path.join(copyFilePath, file));
+        })
+    })
+    fs.readdir(filePath,{withFileTypes: true}, (err, files)=>{
+        files.forEach(file => {
+            fsPromises.copyFile(path.join(filePath, file.name), path.join(copyFilePath, file.name))
+        })
+    })
+}
