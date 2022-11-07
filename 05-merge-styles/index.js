07a94279513489a10f4cd7595794e4ef0808260e
Alex Romanchenko <monsters.alex0@gmail.com>
2022-11-07 13:38:36 +0300

    feat: complete 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+const fsPromises = require('fs').promises;
+
+var filePath = path.join(__dirname, 'project-dist')
+var mergeStylePath = path.join(filePath, 'bundle.css')
+var stylePath = path.join(__dirname, 'styles')
+
+fs.truncateSync(mergeStylePath);
+fs.readdir(stylePath,{withFileTypes: true}, (err, files)=>{
+    files.forEach(file => {
+        if (file.isFile()){
+            fs.stat(path.join(stylePath, file.name), (error, stats)=>{
+                var split = file.name.split('.')
+                if(split[1] == 'css'){
+                    fs.readFile(path.join(stylePath, file.name), 'utf8', (err, data) => {
+                        fs.writeFile(mergeStylePath, data,{flag:'a'} ,  (err) => {})
+                    })
+                }
+            })
+        }
+    })
+})
